6c80f1604493cd9c70b3e37120616920d067a6ad
hilaldfzn <hilalfauzan9@gmail.com>
2024-02-12 10:04:08 +0700

    test: add test for Home Controller (65% overall code coverage)

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
@@ -1,14 +1,13 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
-
+    
     @GetMapping("")
     public String homePage() {
-        return "homePage";
+        return "HomePage";
     }
 }
