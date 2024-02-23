89e78fb62001f2e0a36f17530ea069c2f204d357
hilaldfzn <hilalfauzan9@gmail.com>
2024-02-23 23:54:51 +0700

    refactor: remove public modifier on interface methods

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public Product delete(Product product);
-    public Product edit(Product editedProduct);
-    public Product findById(String productId);
-    public List<Product> findAll();
+    Product create(Product product);
+    Product delete(Product product);
+    Product edit(Product editedProduct);
+    Product findById(String productId);
+    List<Product> findAll();
 }
